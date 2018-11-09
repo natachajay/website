2792811c07e1c12b0a9d178790627dfd52c301d8
Natacha Jensen <natachajay@gmail.com>
2018-11-09 23:33:24 +0100

    game elements, ui and backgrounds added, ready for animation

--- natachajay.dk/kea/03-animation/spil/script.js
+++ natachajay.dk/kea/03-animation/spil/script.js
@@ -4,6 +4,12 @@
 
 showStart
 
+clickStart //Drop start-knappen - brug også til Igorina win-drop, uden klik
+
+// $(document).click(function (event) {        
+//     $('div').toggleClass('drop');
+// });
+
 hideStart
 
 startGame
