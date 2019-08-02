f98fefe91be91e692be72ff19e60db61a89da05e
Natacha Jensen <natachajay@gmail.com>
2019-08-02 15:15:36 +0200

    testing commit

--- natachajay.dk/kea/2-semester/eksamen/header.php
+++ natachajay.dk/kea/2-semester/eksamen/header.php
@@ -10,57 +10,62 @@
     </head>
     
     <body>
-    <!-- HERO AREA -->
-        <div href="index.html" class="logo logo_box">
-        </div>
-
-        <!-- MENU -->
-        <nav class="top_menu">
-            <a href="collection.php">
-                <h4>Collection</h4>
-            </a>
-            <a href="piercing.php">
-                <h4>Piercing</h4>
-            </a>
-            <a href="#" onclick="toggleArticleMenu();">
-                <h4>Our World</h4>
-            </a>
-        </nav>
-
-        <section class="sectionwrapper">
-            <div class="hero_image">
+        <header>
+        <!-- HERO AREA -->
+            <div href="index.html" class="logo logo_box">
             </div>
-        </section>
-    <!-- UNDER HERO AREA -->
-        <!-- COLLECTION FILTERING MENU -->
-        <menu class="filtering_menu"></menu>
+        <!-- MENUS -->
+            <!-- MAIN MENU -->
+            <nav class="top_menu">
+                <div class="top_menu_content_wrapper">
+                    <a href="index.html">
+                        <h4>Home</h4>
+                    </a>
+                    <a href="collection.php">
+                        <h4>Collection</h4>
+                    </a>
+                    <a href="our-world.php" onclick="toggleArticleMenu();">
+                        <h4>Our World</h4>
+                    </a>
+                </div>
+            </nav>
 
-        <template class="filtering_parent_temp">
-            <section class="filter_wrapper">
-                <p class="filter_parent"></p>
-                <div class="filter_children hidden"></div>
+            <section class="sectionwrapper">
+                <div class="hero_image">
+                </div>
             </section>
-        </template>
 
-        <template class="filtering_temp">
-            <p class="filtering_menu_item"></p>
-        </template>
+        <!-- UNDER HERO AREA -->
+            <!-- COLLECTION FILTERING MENU -->
+            <menu class="filtering_menu"></menu>
 
-        <!-- PIERCING MENU -->
-        <menu class="article_menu"></menu>
+            <template class="filtering_parent_temp">
+                <section class="filter_wrapper">
+                    <p class="filter_parent"></p>
+                    <div class="filter_children hidden"></div>
+                </section>
+            </template>
 
-        <template class="piercing_menu_parent_temp">
-            <section class="piercing_menu_wrapper">
-            </section>
-        </template>
+            <template class="filtering_temp">
+                <p class="filtering_menu_item"></p>
+            </template>
 
-        <template class="piercing_menu_item_temp">
-            <p class="piercing_menu_item"></p>
-        </template>
+            <!-- PIERCING MENU -->
+            <menu class="article_menu"></menu>
 
-        <!-- ARTICLE MENU -->
-        <menu class="article_menu"></menu>
+            <template class="piercing_menu_parent_temp">
+                <section class="piercing_menu_wrapper">
+                </section>
+            </template>
 
-        <template class="article_menu_item_temp">
-            <p class="article_menu_item" onclick="loadArticle(this);"></p>
-        </template>
+            <template class="piercing_menu_item_temp">
+                <p class="piercing_menu_item"></p>
+            </template>
+
+            <!-- ARTICLE MENU -->
+            <menu class="article_menu"></menu>
+
+            <template class="article_menu_item_temp">
+                <p class="article_menu_item" onclick="loadArticle(this);"></p>
+            </template>
+        </header>
