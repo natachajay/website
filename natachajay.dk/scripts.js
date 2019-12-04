649d34d85201cbbff333157412d17cf5371fe45f
Natacha Jensen <natachajay@gmail.com>
2019-12-04 16:44:43 +0100

    filtering menu done - working on singleview

--- natachajay.dk/scripts.js
+++ natachajay.dk/scripts.js
@@ -11,11 +11,12 @@ let categoryList = [];
 
 async function initiateProjectLoop() {
     await loadProjects();
+    initiateFilterMenu();
 }
 
 async function loadProjects() {
     // Gather category info
-    let url = baseUrl + "project?per_page=100";
+    let url = baseUrl + "project";
     let jsonData = await fetch(url);
     let data = await jsonData.json();
     
@@ -30,37 +31,122 @@ async function loadProjects() {
         klon.querySelector(".grid_box").dataset.categoryId = categoryId;
         // Add projectId
         klon.querySelector(".grid_box").dataset.projectId = item.id;
-        // Add categoryId to the global variables list
-        if (categoryList.indexOf(categoryId) === -1) {
-            categoryList.push(categoryId)
-        }
         mainElement.appendChild(klon);
     });
 }
 
-async function intitiateFilterMenu() {
-    // Gather category info
-    let catUrl = baseURL + "categories?per_page=100";
+// FILTER MENU
+
+async function initiateFilterMenu() {
+    // Gather all categories
+    let catUrl = baseUrl + "categories";
     let catJsonData = await fetch(catUrl);
     let catData = await catJsonData.json();
-    // Filter all categories through categoryList
-    let filteredList = catData.filter(item => categoryList.includes(item.id));
-    // For every category
-        // Create elm for category in the filter menu with categoryID as data-cat
+    // Remove 'uncategorized' category from list
+    catData = catData.filter(jsonItem => jsonItem.id > 1);
+    // Create element for category in filtering menu with catID as data-cat
     let filterTemplate = document.querySelector(".temp_filtering");
     let filterMenu = document.querySelector(".menu_filtering");
-    filteredList.forEach(function(item) {
-        let clone = filterTemplate.cloneNode(true).content;
-        clone.querySelector(".menu_filtering_item").innerHTML = item.name;
-        clone.querySelector(".menu_filtering_item").dataset.categoryId = item.id;
-        clone.querySelector(".menu_filtering_item").addEventListener("click", function(event) {
+    catData.forEach(function(item) {
+        let klon = filterTemplate.cloneNode(true).content;
+        klon.querySelector(".menu_filtering_item").innerHTML = item.name;
+        klon.querySelector(".menu_filtering_item").dataset.categoryId = item.id;
+        klon.querySelector(".menu_filtering_item").addEventListener("click", function(event) {
             filterClick(this);
-            event.stopPropagation();
-        }, false);
+        });
+        filterMenu.appendChild(klon);
     });
 }
 
-function filterClick(filterElement) {
-    // Finish - what happens when you click a filter_menu_item?
+    function filterClick(filterElement) {
+        // Remove "active" from all filtermenu_items
+        let currentlyActive = document.querySelector(".menu_filtering_item.active");
+        if ( currentlyActive != undefined ) {
+            currentlyActive.classList.remove("active");
+        }
+        if ( currentlyActive == filterElement ) {
+            document.querySelector(".menu_filtering_item.default").click();
+        }
+        else {
+            // Add "active" to this filtermenu_item
+            filterElement.classList.add("active");
+            // Hide all projects
+            let main = document.querySelector("main.content_portfolio");
+            main.querySelectorAll(".grid_box").forEach(project => project.classList.add("hidden"));
+            // Call function to display all corresponding project items
+            displayProjects(filterElement.dataset.categoryId);
+        }
+    }
+
+        function displayProjects(categoryId) {
+            if (categoryId == "all" ) {
+                displayAllProjects();
+            } else {
+                // Show all projects with corresponding categoryId
+                document.querySelectorAll(`.grid_box[data-category-id='${categoryId}']`).forEach(project => project.classList.remove("hidden"));   
+            }
+        }
+
+    function displayAllProjects() {
+        // Show all projects
+        document.querySelectorAll(".grid_box").forEach(project => project.classList.remove("hidden"));
+    }
+
+// SINGLEVIEW (UNDER CONSTRUCTION)
+
+async function loadProjectSingle(project) {
+    // Get project id
+    let currentProject = project.dataset.projectId;
+    // Get project from WP by id
+    let projectUrl = baseUrl + `project/${currentProject}`;
+    let projectJsonData = await fetch(projectUrl);
+    let projectData = await projectJsonData.json();
+    // Create singleView for current project
+    let singleViewElm = document.querySelector(".singleview_sectionwrapper");
+    singleViewElm.querySelector(".project_title").innerHTML = projectData.title.rendered;
+    singleViewElm.querySelector(".project_description").innerHTML = projectData.content.rendered;
+    
+    // Check if project files exist individually
+    
+    // Insert project files
+    
+    singleViewElm.classList.remove("hidden");
 }
 
+function exitSingleView(event) {
+    document.querySelector(".singleview_sectionwrapper").classList.add("hidden");
+}
+
+
+function displayPrevious() {
+    // Find billedet med "vis" klassen og skjul det
+    let currentElm = document.querySelector(".singleview_img_section img.shown");
+    currentElm.classList.remove("shown");
+    // Find billedet der kommer lige inden
+    let prevElm = currentElm.previousElementSibling;
+    // Hvis billedet ikke eksisterer: Find det sidste barn af parent
+    if (prevElm == null) {
+        prevElm = currentElm.parentElement.lastChild;
+    }
+    // Vis det fundne billede
+    prevElm.classList.add("shown");
+}
+
+function displayNext() {
+    // Find billedet med "vis" klassen
+    let currentElm = document.querySelector(".singleview_img_section img.shown");
+    // Skjul billedet
+    currentElm.classList.remove("shown");
+    // Find billedet der kommer lige efter
+    let nextElm = currentElm.nextElementSibling;
+    // Hvis billedet ikke eksisterer: Find det første barn af parent
+    if (nextElm == null) {
+        nextElm = currentElm.parentElement.firstChild;
+    }
+    // Vis det fundne billede
+    nextElm.classList.add("shown");
+}
+
+
+
+// END OF PORTFOLIO
