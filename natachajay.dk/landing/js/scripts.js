fad33602a4d707b0ce11ba652e27cd63463587ec
Natacha Jensen <natachajay@gmail.com>
2019-12-07 21:43:21 +0100

    full singleview completed - final touches

--- natachajay.dk/landing/js/scripts.js
+++ natachajay.dk/landing/js/scripts.js
@@ -16,6 +16,10 @@ function initiateUpBtn() {
             upBtn.classList.add("hidden");
         }
     });
+    
+    window.addEventListener("resize", function() {
+        tippingPoint = window.innerHeight;
+    });
 }
 
 // PORTFOLIO
@@ -25,6 +29,7 @@ function initiateUpBtn() {
 var baseUrl = "https://natachajay.dk/wordpress/wp-json/wp/v2/";
 var categoryList = [];
 var projectFiles;
+const videoCategory = 4;
 
 async function initiateProjectLoop() {
     await loadProjects();
@@ -43,6 +48,12 @@ async function loadProjects() {
         // Add to list of projects
         let klon = loopTemplate.cloneNode(true).content;
         klon.querySelector(".grid_box").style.backgroundImage = `url('${item.cover_image.guid}')`;
+        // Check to see if project_files is empty (false)
+        // Contains an array of JSON objects if not empty, otherwise boolean false
+        if ( item.project_files !== false ) {
+            // Not empty - insert icon
+            klon.querySelector(".grid_box").innerHTML = '<img src="https://www.natachajay.dk/landing/media/icons/multiple_files.svg" alt="Multiple Files" class="multiple_icon">';
+        }
         // Add categoryID as data-category
         let categoryId = item.categories[0];
         klon.querySelector(".grid_box").dataset.categoryId = categoryId;
@@ -118,34 +129,72 @@ async function loadProjectSingle(project) {
     let projectUrl = baseUrl + `project/${currentProject}`;
     let projectJsonData = await fetch(projectUrl);
     let projectData = await projectJsonData.json();
-    
+    let singleViewElm;
     // Create singleView for current project
-    let singleViewElm = document.querySelector(".singleview_sectionwrapper");
+    if ( projectData.categories[0] == videoCategory ) {
+        singleViewElm = document.querySelector("#video_singleview");
+        loadProjectSingleVideo(projectData);
+    }
+    else {
+        singleViewElm = document.querySelector("#default_singleview");
+        loadProjectSingleDefault(projectData);
+    }
     singleViewElm.querySelector(".project_title").innerHTML = projectData.title.rendered;
     singleViewElm.querySelector(".project_description").innerHTML = projectData.content.rendered;
-    
-    // Get project files from array?
-    projectFiles = projectData.project_files;
-    let coverEntry = {"guid":projectData.cover_image.guid};
-    projectFiles.unshift(coverEntry);
-    
-    // Insert cover-img first
-    var singleViewImg = document.querySelector(".singleview_img");
-    singleViewImg.alt = `${projectData.title.rendered}`;
-    singleViewImg.dataset.position = 0;
-    
-    singleViewImg.src = projectFiles[0].guid;
     
     singleViewElm.classList.remove("hidden");
 }
 
-function exitSingleView(event) {
-    document.querySelector(".singleview_sectionwrapper").classList.add("hidden");
+function loadProjectSingleDefault(projectData) {
+    // Get project files from array?
+    projectFiles = projectData.project_files;
+    let coverEntry = {"guid":projectData.cover_image.guid};
+    
+    // Insert cover-img first
+    var singleViewImg = document.querySelector(".singleview_img");
+    singleViewImg.alt = `${projectData.title.rendered}`;
+    
+    if ( projectFiles !== false ) {
+        projectFiles.unshift(coverEntry);
+        singleViewImg.src = projectFiles[0].guid;
+        singleViewImg.dataset.position = 0;
+        showArrows();
+    }
+    else {
+        singleViewImg.src = coverEntry.guid;
+        hideArrows();
+    }
+}
+
+function loadProjectSingleVideo(projectData) {
+    document.querySelector('#video_singleview_iframe_wrapper').innerHTML = `<iframe width="853" height="505" src="https://www.youtube.com/embed/${projectData.youtube_id}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+    document.querySelector('#video_singleview').classList.remove("hidden");
+}
+
+function hideArrows() {
+    document.querySelector('#display_previous').classList.add('hidden');
+    document.querySelector('#display_next').classList.add('hidden');
+}
+
+function showArrows() {
+    document.querySelector('#display_previous').classList.remove('hidden');
+    document.querySelector('#display_next').classList.remove('hidden');
+}
+
+function exitSingleView(element) {
+    element.closest(".singleview").classList.add("hidden");
+    // Just in case
+    projectFiles = false;
+    document.querySelector('#video_singleview_iframe_wrapper').innerHTML = "";
 }
 
 // TO DO
 
 function displayPrevious() {
+    if ( projectFiles === false ) {
+        return;
+    }
+    
     let singleViewImg = document.querySelector(".singleview_img");
     let singleViewImgPos = --singleViewImg.dataset.position;
     
@@ -158,6 +207,9 @@ function displayPrevious() {
 }
 
 function displayNext() {
+    if ( projectFiles === false ) {
+        return;
+    }
     let singleViewImg = document.querySelector(".singleview_img");
     let singleViewImgPos = ++singleViewImg.dataset.position;
     
